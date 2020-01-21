84636d4844d57446835cf99f33ddeffa247ae133
Surjit Kumar Sahoo <surjitsahoo0@gmail.com>
2020-01-21 19:08:27 +0530

    fixed issues

--- src/practice.js
+++ src/practice.js
@@ -7,19 +7,9 @@
  */
 
 exports.numbersArray = (n) => {
-  // Your code goes here
   const res = [];
-  for (let i = 1; i <= n; i += 1) {
-    if (i % 3 === 0 && i % 5 !== 0) {
-      res.push('Fizz');
-    } else if (i % 5 === 0 && i % 3 !== 0) {
-      res.push('Buzz');
-    } else if (i % 5 === 0 && i % 3 === 0) {
-      res.push('FizzBuzz');
-    } else {
-      res.push(i);
-    }
-  }
+  // Your code goes here
+  
   return res;
 };
 
@@ -34,16 +24,5 @@ exports.secondLargest = (nums) => {
 
 
 exports.vowelsAndConsonants = (s) => {
-  const vowels = 'aeiou';
-  const consonantPart = [];
-  const vowelPart = [];
-  for (let i = 0; i < s.length; i += 1) {
-    if (vowels.includes(s.charAt(i))) {
-      vowelPart.push(s.charAt(i));
-    } else {
-      consonantPart.push(s.charAt(i));
-    }
-  }
-  console.log(vowelPart.join('\n'));
-  console.log(consonantPart.join('\n'));
+  // Your code goes here
 };
